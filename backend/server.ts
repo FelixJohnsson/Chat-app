ca7acc22415391630da01a7990aa1ad3c6953f17
Felix <felix@capchap.se>
2022-10-14 18:32:34 +0200

    Changed frontend to TS

---
+++ backend/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import 'dotenv/config'
+import axios from 'axios'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
+
+import print from './print'
+
+const app = express()
+express.Router()
+
+app.use(express.static("public"))
+	.use(cors())
+	.use(cookieParser())
+	.use(bodyParser.urlencoded({
+		extended: true
+	}))
+	.use(bodyParser.json())
+
+const port = process.env.PORT
